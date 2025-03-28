95775259fd1c1bad0f9ffa2b690cd28b3adc14f7
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-03-28 16:41:42 +0200

    app2 using express for GET & POST requests, as well as middleware functions

---
+++ myapp2/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+// const bodyParser = require('body-parser')
+// app.use(bodyParser.json());
+// app.user(bodyParser.urlencoded({extended:false}));
+
+app.use(express.json());  // το .use() είναι για να τρέχει ενδιάμεσες συναρτήσεις
+app.use(express.urlencoded({extended:true}))  // αποκωδικοποιεί τα περιεχόμενα φόρμας στους headers (πχ. σύμβολα) για να διαβαστούν ως κείμενο
+
+app.use('/', express.static('files'));  // μόλις γίνει κλήση στο root('/'), ανοίγουν τα στατικά αρχεία στον φάκελο files
+
+app.get('/', (req, res)=>{
+  res.send("This is the home page");
+});
+
+app.post('/user', (req, res) => {
+  let data = req.body;
+  let username = req.body.username;
+  let email = req.body.email;
+
+  console.log(data);
+  res.send(`User data ${username}, ${email}`);
+});
+
+app.post('/userForm', (req, res)=>{
+  let data = req.body
+
+  console.log("Data", data);
+  res.send("UserForm Page");
+});
+
+app.use('/user1', (req, res) => {
+  console.log("User 1")
+  res.send("User 1 Page");
+});
+
+app.use('/user2', (req, res) => {
+  console.log("User 2")
+  res.send("User 2 Page");
+});
+
+/**
+ * Θα αγνοηθεί επειδή το route /user2 ικανοποιεί το app.use() (short-circuit)
+ */
+app.use('/user2/hello', (req, res) => {
+  console.log("User 2 Hello")
+  res.send("User 2 Page Hello");
+})
+
+app.listen(port, ()=>{
+  console.log("Server is running on port 3000.");
+});
